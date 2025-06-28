6bd97ef97b12db4a6e0da349af5a09e1325a0702
mrcharan21 <charanguguloth13@gmail.com>
2025-06-28 19:24:47 +0530

    u

---
+++ module4_reactjs/how_to_create_app_react_js/app_components/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CalcApp from './components/CalcApp';
+import ContactUS from './components/ContactUs';
+import EmployeeApp from './components/EmployeeApp';
+
+
+
+export default function App() {
+  return (
+    <div>
+        <CalcApp />
+        <ContactUS />
+        <EmployeeApp />
+
+    </div>
+  );
+}
