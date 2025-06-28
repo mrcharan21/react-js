208cb0b217c1ea32008f08d8389dcb999e880901
mrcharan21 <charanguguloth13@gmail.com>
2025-06-28 19:26:04 +0530

    up

--- module4_reactjs/how_to_create_app_react_js/app_components/src/index.js
+++ module4_reactjs/how_to_create_app_react_js/app_components/src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import EmployeeApp from "./components/EmployeeApp";
-import CalcApp from "./components/CalcApp";
-import ContactUs from "./components/ContactUs";
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-       <EmployeeApp />
-       <CalcApp />
-       <ContactUs />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(<App / >);
 
-)
+
+
