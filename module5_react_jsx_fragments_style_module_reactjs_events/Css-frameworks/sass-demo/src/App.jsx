cd7e6bedea8cb24ca200ad0f7adfb34849a34efd
mrcharan21 <charanguguloth13@gmail.com>
2025-07-30 11:24:54 +0530

    folder updated

---
+++ module5_react_jsx_fragments_style_module_reactjs_events/Css-frameworks/sass-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+    <div className='app'>
+      <h1> This is Sass demo </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem quia culpa illo corrupti laboriosam tempora incidunt quidem natus excepturi.</p>
+    </div>
+    </>
+  )
+}
