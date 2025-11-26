4b6c0f7c126ebf271f317175a8a8fdde95ce9255
mrcharan21 <89964531+mrcharan21@users.noreply.github.com>
2025-11-26 12:44:59 +0530

    updated

--- module7_components_states_props/state-demo-app/src/Components/ArrayState.jsx
+++ module7_components_states_props/state-demo-app/src/Components/ArrayState.jsx
@@ -4,7 +4,6 @@ export default function ArrayState() {
     const[items, setItems] = useState(['1']);
   return (
     <>
-    <button onClick={()=> setItems([...items, `Item ${items.length + 1}`])}>Add item</button>
     <ul>
         {items.map((i, index ,)=>
         <li key={index}>{i}</li>)}
